bb658d78b4b4df427d5a4dcf8d1ab69f7cba2d87
Nisarg-Satani <nisargsatani2@gmail.com>
2021-08-18 12:14:06 +0530

    changed code

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 # Importing required libraries
+import git
+from git import Repo
 from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify
 import os
-import shutil
 
 app = Flask(__name__)
-# base_dir = '/var/projects'
 base_dir = os.getcwd()
-print(base_dir)
-# exit()
+
 
 @app.route("/", methods= ['GET'])
 def show():
@@ -20,14 +19,14 @@ def submit():
     :return: renders the index.html with parsed data
     """
     if request.method == "POST":
-        projectname= request.form.get("projectname")
+        Project_name = request.form.get("Project_name")
         Authenticate = request.form.get("Authenticate")
         Git_Url = request.form.get("Git_Url")
         Branch_Name = request.form.get("Branch_Name")
         username = request.form.get("username")
         token = request.form.get("token")
         user_data = {
-            "projectname":projectname,
+            "Project_name":Project_name,
             "Authenticate":Authenticate,
             "Git_Url":Git_Url,
             "Branch_Name":Branch_Name,
@@ -36,39 +35,34 @@ def submit():
             
         }
         print(user_data)
-    
-    
-       
-        
-       
-       # request_url = f'git clone -b {Branch_Name} @{Git_Url}' /var/projects/projectname
-        # request_url = f'git clone -b {Branch_Name}  https://{username}:{token} @{Git_Url}'
         
         if Authenticate == "GitHub" or "GitLab":
-            u = Git_Url.split("//")
-            url = u[1]
-            # 'https://github.com/Nisarg-Satani/ChatBro.git'
-            repo_name = u[1].split("/")[-1].split(".")[0]
-            print("repo name:", repo_name)
+            u=Git_Url.split("//")
+            url=u[1]
+       # request_url = f'git clone -b {Branch_Name} @{Git_Url}'
+        # request_url = f'git clone -b {Branch_Name}  https://{username}:{token} @{Git_Url}'
+       
+        request_url = f"https://{username}:{token}@{url}"
+        
+        parent_dir="F:/var/Project/"
 
-            request_url = f'git clone -b {Branch_Name} https://{username}:{token}@{url}'
-
-            if not os.path.exists(os.path.join(base_dir + f"/{projectname}")):
-                print("inside mkdir")
-                os.mkdir(f'{projectname}')
-            if not os.path.exists(f"{repo_name}"):
-                os.system(f"{request_url}")
-                shutil.move(f'{repo_name}', f'{projectname}')
-                return jsonify({
-                    "status": 200,
-                    "message": f"{Authenticate} has been cloned!"
-                })
-            else:
-                    return jsonify({
-                        "status": 502,
-                        "message": f"{url} already there!"
-                    })
-        return render_template("index.html")
+        path = os.path.join(parent_dir,Project_name)
+        
+        os.mkdir(path)
+        
+        if  os.path.exists(path):
+            Repo.clone_from(request_url,path, branch=Branch_Name)
+            os.system(f"{request_url}")
+            return jsonify({
+                "status": 200,
+                "message": f"{url} has been cloned!"
+            })
+        else:
+            return jsonify({
+                "status": 502,
+                "message": f"{url} already there!"
+            })
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
